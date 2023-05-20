f02062a093dd1b3bcf0e9548a79d2cad717356b2
Daniel Muñoz <daniel20025febrero29@gmail.com>
2023-05-20 10:53:34 -0500

    Modify code and remove

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,7 @@ function App() {
       <h1>Welcome to design 3D in React</h1>
       <div className='div-spline-viewer'>
         <spline-viewer className='spline-viewer' url="https://prod.spline.design/oRI7Fal11-Y2qygG/scene.splinecode"></spline-viewer>
-      </div>
-      {/* <div className='div-spline-phone'>
-        <spline-viewer url="https://prod.spline.design/rWJ8tU5t0eLWV0K2/scene.splinecode"></spline-viewer>
-      </div> */}
+      </div>      
     </>
   )
 }
