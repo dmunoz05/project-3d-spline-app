6c8f498791c70d31316180aa46f843e9b5635de4
Daniel Muñoz <daniel20025febrero29@gmail.com>
2023-05-20 10:51:07 -0500

    Design using library Spline and code of design

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ function App() {
 
   return (
     <>
-    <h1>Welcome to design 3D in React</h1>
+      <h1>Welcome to design 3D in React</h1>
+      <div className='div-spline-viewer'>
+        <spline-viewer className='spline-viewer' url="https://prod.spline.design/oRI7Fal11-Y2qygG/scene.splinecode"></spline-viewer>
+      </div>
+      {/* <div className='div-spline-phone'>
+        <spline-viewer url="https://prod.spline.design/rWJ8tU5t0eLWV0K2/scene.splinecode"></spline-viewer>
+      </div> */}
     </>
   )
 }
