2f37f19530e6dce6be318b0578445311d7c6e30f
Daniel Muñoz <daniel20025febrero29@gmail.com>
2023-05-20 22:25:13 -0500

    Config Vite Deploy gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/project-3d-spline-app/',
+  plugins: [react()]
 })
